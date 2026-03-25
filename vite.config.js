ca315ddbfde9e72f8141e84348f04b1fec6d51c7
Vadim Gushchin <gushchinvadim@gmail.com>
2026-03-25 17:11:34 +0300

    3 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/filter/', // ← Критично для GitHub Pages
+  
+  plugins: [
+    react()
+    // babel + React Compiler временно отключены для стабильности
+  ],
 })
